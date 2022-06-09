874c40371e03ea7ec6407254e8d20e5ef88ecff1
gassara-kys <ogasawara_kiyoshi@cybernagent>
2022-06-09 15:34:44 +0900

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ogasawarayzer
+
+## required 
+
+- docker
---
+++ main.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+RESULT_FILE_PATH="/tmp/result.json"
+INPUT_FILE_PATH="./url.txt"
+
+function init () {
+    echo -n "" > ${RESULT_FILE_PATH}
+}
+
+function scan () {
+    echo "Taget URL: $1"
+    docker run public.ecr.aws/risken/osint/website-base:v0.0.1 /opt/wappalyzer/src/drivers/npm/cli.js $1 >> ${RESULT_FILE_PATH}
+}
+
+init
+while read url
+do
+  # call scan
+  if [[ ${url} == "http"* ]]
+  then
+    scan $url
+  else 
+    scan http://${url}
+    scan https://${url}
+  fi
+done < ${INPUT_FILE_PATH}
+
+cat ${RESULT_FILE_PATH}
